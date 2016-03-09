b277a24f17ec81ca4d3fb3d56cd168f00371f7cf
crystalynliu <crystalynliu@163.com>
2016-03-09 23:54:24 +0800

    finish loadReceipt

---
+++ pos-web/js/loadReceipt.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  var receipt = getStorage('tempReceipt');
+
+  receipt.cart.forEach(function(cartItem){
+    var item = findItem(cartItem.itemId);
+    var tr = "<tr><td>"
+        +item.name+"</td><td>"+item.price
+        +"</td><td>"+cartItem.count+"</td><td>"
+        +item.unit+"</td><td name='subtotal'>"
+        +cartItem.subtotal+"</td></tr>";
+    $("#cartlist").append(tr);
+  })
+  $('#cartCount').text(receipt.cart.length);
+  $('#totalprice').text("$"+receipt.totalprice);
+  var tradeRecord = getStorage("Receipts");
+  $('#recordCount').text(tradeRecord.length);
+})
